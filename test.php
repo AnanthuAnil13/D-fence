87aef2aef8ad4686e77925f963b735cb20424af7
AnanthuAnil13 <42567444+AnanthuAnil13@users.noreply.github.com>
2022-06-07 22:02:00 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,37 +1,11 @@
 <?php
-	header("Access-Control-Allow-Origin: *");
-	header("Access-Control-Allow-Headers: *");
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Headers: *");
 
-	$url = $_GET['senturl'];
-	$output = shell_exec('sudo /var/www/html/docker.sh "'.$url.'"');	
-	echo $output;
-	//$dockerurl = "http://25.9.115.112:".$output[1];
-	//echo $dockerurl;
+        $url = $_GET['senturl'];
+        $ip = $_GET['sentip'];
 
-	//var_dump($_REQUEST);
-	//$ip = "255.255.255.255";
-	//$servername = "localhost";
-	//$username = "username";
-	//$password = "Password123@";
-	//$dbname = "urldb";
+        $output = shell_exec('sudo /var/www/html/docker.sh "'.$url.'" "'.$ip.'"');
 
-	// Create Connection
-	//$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-	// Check connection
-	//if (!$conn) {
-//		die("Connection Failed: " . mysqli_connect_error());
-//	}
-
-//	$sql = "INSERT INTO Link_Table (link, ip) VALUES (".$recievedurl.",".$ip.")";
-//	try {
-//		if (mysqli_query($conn, $sql)) {
-//			var_dump("New record created successfully");
-//		} else {
-//			var_dump("DB Entry Error");
-//		}
-//	} catch(Exception $e) {
-//		var_dump("Try_Catch Error");
-//	}
-//	mysqli_close($conn);
+        echo $output;
 ?>
