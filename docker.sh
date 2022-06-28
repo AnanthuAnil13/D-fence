3776d6844418400f1db87093a48e659c978626db
AnanthuAnil13 <42567444+AnanthuAnil13@users.noreply.github.com>
2022-06-28 12:03:37 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -4,9 +4,10 @@ port=$((46337))
 url=$1
 ip=$2
 
-mysql  --defaults-extra-file=./config.cnf urldb -e 'INSERT INTO Link_Table (link, ip, port) VALUES ("'$url'", "'$ip'", '$port')'
+mysql --defaults-extra-file=./config.cnf urldb -e 'INSERT INTO Link_Table (link, ip, port) VALUES ("'$url'", "'$ip'", '$port')'
 
 name="firefox-$port"
-#echo $port
 
-sudo docker run -d --name=$name -p $port:5800 --shm-size 2g -e "FF_PREF_HTTP_PROXY=startup.homepage_welcome_url=\"$url\"" jlesage/firefox
+sudo docker run -d --name=$name -p $port:5800 --shm-size 2g -e "FF_PREF_HTTP_PROXY=startup.homepage_welcome_url=\"$url\"" -e "VNC_PASSWORD=12345678" ananthuanil/d-fence
+
+echo $port
